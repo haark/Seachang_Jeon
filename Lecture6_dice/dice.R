6edffe2afb4329abb73c0892c556980b4d68d1b9
Seachang Jeon <haark27@gmail.com>
2015-07-06 11:27:47 +0900

    lecture6

--- Lecture6_dice/dice.R
+++ Lecture6_dice/dice.R
@@ -1,19 +1,7 @@
-r=10
-d1 <- sample(6,r,replace=T)
-d2 <- sample(6,r,replace=T)
-dd <- d1+d2
-hist(dd,freq = false)
- Error in hist.default(dd, freq = false) : object 'false' not found
-hist(dd,freq = FALSE)
-r=100
-d3 <- sample(6,r,replace=T)
-d4 <- sample(6,r,replace=T)
-ddd <- d3+d4
-hist(ddd, freq = FALSE)
-hist(dd, freq = FALSE)
-r=100
 r=10000
-d5 <- sample(6,r,replace=T)
+d5 <- sample(6,r,replace=T
+             )
 d6 <- sample(6,r,replace=T)
-dddd <- d5+d6
+dddd <-d5+d6
 hist(dddd, freq = FALSE)
+plot.ecdf(dddd)
---
+++ Lecture6_dice/pnorm_rnorm.R
@@ -0,0 +1,7 @@
+> n<-rnorm(1000)
+> hist(n)
+> hist(rnorm(10000,mean=10,sd=5))
+> z <-(67.71-70)/5
+> pnorm(z, mean=70, sd=5, lower.tail=TRUE, log.p=FALSE)
+[1] 2.138795e-45
+> 
